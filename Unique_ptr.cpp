e8162c712ea362243fb556aa0cc16fd6af23a21d
Hayk Manukyan <70444049+pmimanukyan@users.noreply.github.com>
2021-10-16 22:21:26 +0300

    Update Unique_ptr.cpp

--- Unique_ptr.cpp
+++ Unique_ptr.cpp
@@ -1,85 +1,269 @@
-#include <algorithm>
-#include <cstddef>
-#include <memory>
-#include <tuple>
+#pragma once
 
-template <typename T, typename Deleter = std::default_delete<T>>
+#include "compressed_pair.h"
+#include <utility>
+#include <functional>
+#include <cstddef>  // std::nullptr_t
+
+template <typename T>
+struct Slug {
+    void operator()(T* ptr) {
+        delete ptr;
+    }
+};
+
+template <typename Y>
+struct Slug<Y[]> {
+    void operator()(Y* ptr) {
+        delete[] ptr;
+    }
+};
+// Primary template
+template <typename T, typename Deleter = Slug<T>>
 class UniquePtr {
-    std::tuple<T*, Deleter> data;
-
 public:
-    UniquePtr() noexcept {
-        std::get<0>(data) = nullptr;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Constructors
+
+    explicit UniquePtr(T* ptr = nullptr) {
+        pair_.GetFirst() = ptr;
     }
 
-    UniquePtr(T* ptr) noexcept {
-        std::get<0>(data) = ptr;
+    template <typename Deleter1>
+    UniquePtr(T* ptr, Deleter1&& deleter) noexcept : pair_(ptr, std::forward<Deleter1>(deleter)) {
     }
 
-    UniquePtr(T* ptr, Deleter deleter) noexcept {
-        std::get<0>(data) = ptr;
-        std::get<1>(data) = deleter;
+    template <typename S, typename SDeleter>
+    UniquePtr(UniquePtr<S, SDeleter>&& other) noexcept
+        : pair_(other.pair_.GetFirst(), std::forward<SDeleter>(other.pair_.GetSecond())) {
+        other.pair_.GetFirst() = nullptr;
     }
 
-    UniquePtr(const UniquePtr&) = delete;
-
-    UniquePtr(UniquePtr&& that) noexcept {
-        swap(that);
+    UniquePtr(UniquePtr&& other) noexcept {
+        pair_.GetFirst() = other.pair_.GetFirst();
+        pair_.GetSecond() = std::forward<Deleter>(other.pair_.GetSecond());
+        other.pair_.GetFirst() = nullptr;
     }
 
-    UniquePtr& operator=(std::nullptr_t) noexcept {
-        std::get<1>(data)(std::get<0>(data));
-        std::get<0>(data) = nullptr;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // `operator=`-s
+
+    UniquePtr& operator=(UniquePtr&& other) noexcept {
+        if (other.pair_.GetFirst() == pair_.GetFirst()) {
+            return *this;
+        } else {
+            pair_.GetSecond()(pair_.GetFirst());
+            pair_.GetFirst() = nullptr;
+            std::swap(pair_.GetFirst(), other.pair_.GetFirst());
+            pair_.GetSecond() = std::forward<Deleter>(other.pair_.GetSecond());
+            return *this;
+        }
+    }
+
+    UniquePtr& operator=(std::nullptr_t other) {
+        delete pair_.GetFirst();
+        pair_.GetFirst() = other;
         return *this;
     }
 
-    UniquePtr& operator=(const UniquePtr&) = delete;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Destructor
 
-    UniquePtr& operator=(UniquePtr&& rhs) noexcept {
-        swap(rhs);
-        return *this;
-    }
+    ~UniquePtr() {
+        pair_.GetSecond()(pair_.GetFirst());
+    };
 
-    T& operator*() const noexcept {
-        return *std::get<0>(data);
-    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Modifiers
 
-    T* operator->() const noexcept {
-        return std::get<0>(data);
-    }
-
-    T* release() noexcept {
-        T* ptr = nullptr;
-        std::swap(std::get<0>(data), ptr);
+    T* Release() {
+        T* ptr = pair_.GetFirst();
+        pair_.GetFirst() = nullptr;
         return ptr;
     }
-
-    void reset(T* ptr) noexcept {
-        std::get<1>(data)(std::get<0>(data));
-        std::swap(std::get<0>(data), ptr);
+    
+    void Reset(T* ptr = nullptr) {
+        UniquePtr tmp(pair_.GetFirst());
+        pair_.GetFirst() = ptr;
     }
 
-    void swap(UniquePtr& rhs) noexcept {
-        std::swap(std::get<0>(data), std::get<0>(rhs.data));
+    void Swap(UniquePtr& other) {
+        std::swap(pair_.GetFirst(), other.pair_.GetFirst());
+        std::swap(pair_.GetSecond(), other.pair_.GetSecond());
     }
 
-    T* get() const noexcept {
-        return std::get<0>(data);
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Observers
+
+    T* Get() const {
+        return pair_.GetFirst();
     }
 
-    explicit operator bool() const noexcept {
-        return std::get<0>(data) != nullptr;
+    T*& GetPtr() {
+        return pair_.GetFirst();
     }
 
-    Deleter& get_deleter() noexcept {
-        return std::get<1>(data);
+    T* GetPtr() const {
+        return pair_.GetFirst();
+    }
+    
+    Deleter& GetDeleter() {
+        return pair_.GetSecond();
+    }
+    
+    const Deleter& GetDeleter() const {
+        return pair_.GetSecond();
+    }
+    
+    explicit operator bool() const {
+        if (pair_.GetFirst() == nullptr) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
-    const Deleter& get_deleter() const noexcept {
-        return std::get<1>(data);
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Single-object dereference operators
+
+    typename std::add_lvalue_reference<T>::type operator*() const {
+        return *pair_.GetFirst();
+    }
+    
+    T* operator->() const {
+        return pair_.GetFirst();
     }
 
-    ~UniquePtr() noexcept {
-        std::get<1>(data)(std::get<0>(data));
+private:
+    CompressedPair<T*, Deleter> pair_;
+    template <typename U, typename Q>
+    friend class UniquePtr;
+};
+
+//// Specialization for arrays
+template <typename T, typename Deleter>
+class UniquePtr<T[], Deleter> {
+public:
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Constructors
+
+    explicit UniquePtr(T* ptr = nullptr) {
+        pair_.GetFirst() = ptr;
     }
+
+    template <typename Deleter1>
+    UniquePtr(T* ptr, Deleter1&& deleter) noexcept : pair_(ptr, std::forward<Deleter1>(deleter)) {
+    }
+
+    template <typename S, typename SDeleter>
+    UniquePtr(UniquePtr<S, SDeleter>&& other) noexcept {
+        if (pair_.GetFirst() != other.pair_.GetFirst()) {
+            pair_.GetFirst() = other.pair_.GetFirst();
+            pair_.GetSecond() = std::forward<SDeleter>(other.pair_.GetSecond());
+            other.Release();
+        }
+    }
+
+    UniquePtr(UniquePtr&& other) noexcept {
+        pair_.GetFirst() = other.pair_.GetFirst();
+        pair_.GetSecond() = std::forward<Deleter>(other.pair_.GetSecond());
+        other.pair_.GetFirst() = nullptr;
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // `operator=`-s
+
+    UniquePtr& operator=(UniquePtr&& other) noexcept {
+        if (other.pair_.GetFirst() == pair_.GetFirst()) {
+            return *this;
+        } else {
+            pair_.GetSecond()(pair_.GetFirst());
+            pair_.GetFirst() = nullptr;
+            std::swap(pair_.GetFirst(), other.pair_.GetFirst());
+            pair_.GetSecond() = std::forward<Deleter>(other.pair_.GetSecond());
+            return *this;
+        }
+    }
+
+    UniquePtr& operator=(std::nullptr_t other) {
+        delete pair_.GetFirst();
+        pair_.GetFirst() = other;
+        return *this;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Destructor
+
+    ~UniquePtr() {
+        pair_.GetSecond()(pair_.GetFirst());
+    };
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Modifiers
+
+    T* Release() {
+        T* ptr = pair_.GetFirst();
+        pair_.GetFirst() = nullptr;
+        return ptr;
+    }
+    
+    void Reset(T* ptr = nullptr) {
+        UniquePtr tmp(pair_.GetFirst());
+        pair_.GetFirst() = ptr;
+    }
+
+    void Swap(UniquePtr& other) {
+        std::swap(pair_.GetFirst(), other.pair_.GetFirst());
+        std::swap(pair_.GetSecond(), other.pair_.GetSecond());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Observers
+
+    T* Get() const {
+        return pair_.GetFirst();
+    }
+
+    T*& GetPtr() {
+        return pair_.GetFirst();
+    }
+
+    T* GetPtr() const {
+        return pair_.GetFirst();
+    }
+    Deleter& GetDeleter() {
+        return pair_.GetSecond();
+    }
+
+    const Deleter& GetDeleter() const {
+        return pair_.GetSecond();
+    }
+
+    explicit operator bool() const {
+        if (pair_.GetFirst() == nullptr) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Single-object dereference operators
+
+    typename std::add_lvalue_reference<T>::type operator*() const {
+        return *pair_.GetFirst();
+    }
+
+    T* operator->() const {
+        return pair_.GetFirst();
+    }
+
+    T& operator[](size_t i) {
+        return pair_.GetFirst()[i];
+    }
+
+private:
+    CompressedPair<T*, Deleter> pair_;
+    template <typename U, typename Q>
+    friend class UniquePtr;
 };
